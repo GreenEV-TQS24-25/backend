97031d6b70d48e0e5331272efd4c870b22af5156
VioletaR <violetabatistaramos@gmail.com>
2025-05-16 09:58:52 +0100

    SPOKY SCARY SKELETON THE REAL ONE

---
+++ src/main/java/ua/deti/tqs/services/interfaces/UserTableService.java
@@ -0,0 +1,13 @@
+package ua.deti.tqs.services.interfaces;
+
+import ua.deti.tqs.entities.UserTable;
+
+public interface UserTableService {
+    UserTable getUserById(int id);
+
+    UserTable createUser(UserTable user);
+
+    UserTable updateUser(int id, UserTable user);
+
+    boolean deleteUser(int id);
+}
