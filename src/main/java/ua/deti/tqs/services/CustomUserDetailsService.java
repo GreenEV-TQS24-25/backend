0d386b7b78a7633f540c18298be21203447490b1
andre <andrepedro2004@hotmail.com>
2025-05-18 16:33:33 +0100

    useless import

--- src/main/java/ua/deti/tqs/services/CustomUserDetailsService.java
+++ src/main/java/ua/deti/tqs/services/CustomUserDetailsService.java
@@ -1,7 +1,6 @@
 package ua.deti.tqs.services;
 
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -12,10 +11,12 @@ import ua.deti.tqs.repositories.UserRepository;
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
-    private UserRepository userRepository;
+  private UserRepository userRepository;
 
-    @Override
-    public UserDetails loadUserByUsername(String email) {
-        return userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("User não encontrado: " + email));
-    }
+  @Override
+  public UserDetails loadUserByUsername(String email) {
+    return userRepository
+        .findByEmail(email)
+        .orElseThrow(() -> new UsernameNotFoundException("User não encontrado: " + email));
+  }
 }
